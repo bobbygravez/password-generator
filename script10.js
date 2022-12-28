1c67b6a1b87d6c039b207ae6df349539b7b8eb36
Godswill Chidera <bobbylivinus@gmail.com>
2022-12-29 00:53:00 +0100

    done

--- script10.js
+++ script10.js
@@ -8,15 +8,25 @@ const characters = ["A","B","C","D","E","F","G","H","I","j","K","L","M",
 
 const displayOne = document.querySelector("#input-field1")
 const displayTwo = document.querySelector("#input-field2")
+const displayThree = document.querySelector("#input-field3")
+const displayFour = document.querySelector("#input-field4")
 const generateBtn = document.querySelector("#Generate-btn")
 
 generateBtn.addEventListener("click", function () {
+	displayOne.value = " "
+	displayTwo.value = " "
+	displayThree.value = " "
+	displayFour.value = " "
 	for (let i = 0; i < 15; i++) {
 		let generateRandomNumber1 = Math.floor(Math.random() * 89)
 		let generateRandomNumber2 = Math.floor(Math.random() * 89)
+		let generateRandomNumber3 = Math.floor(Math.random() * 89)
+		let generateRandomNumber4 = Math.floor(Math.random() * 89)
 
 				displayOne.value +=  characters[generateRandomNumber1] 
 				displayTwo.value +=  characters[generateRandomNumber2] 
+				displayThree.value +=  characters[generateRandomNumber3] 
+				displayFour.value +=  characters[generateRandomNumber4] 
 			}	
 })				
  
